f1c907dbaa66a027e21a2aa1f2a8b68e05950970
AhmedMuhammedvip <Shizophrenia51@gmail.com>
2023-04-05 16:51:15 -0700

    g

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+void print_alphabet(void)
+{
+char x = 'A';
+
+while (x <= 'Z')
+{
+	_putchar(x);
+	_putchar('\n');
+	x++;
+}
+}
+int main(void)
+{
+print_alphabet();
+return (0);
+}
