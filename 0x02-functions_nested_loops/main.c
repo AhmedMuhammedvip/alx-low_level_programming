e76e28b8a603748497c908d886ab081694ea57fb
AhmedMuhammedvip <Shizophrenia51@gmail.com>
2023-04-05 17:32:22 -0700

    hhghgh

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,10 +1,21 @@
+#include<stdio.h>
 #include "main.h"
-
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
+* main - check the code
+*
+* Return: Always 0.
+*/
+void print_alphabet(void)
+{
+char x = 'A';
+
+while (x <= 'Z')
+{
+	_putchar(x);
+	_putchar('\n');
+	x++;
+}
+}
 int main(void)
 {
 	print_alphabet();
