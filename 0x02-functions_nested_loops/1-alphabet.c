5ecfdd86c75a2fadcb094c1342d50aa77be2409c
AhmedMuhammedvip <Shizophrenia51@gmail.com>
2023-04-07 12:29:56 -0700

    now working ??

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * print_alphabet - utilizes on the _puthcar function to print 
+ * 			the alphabet a - z
+ */ 			 
+void print_alphabet(void)
+{
+
+char x = 'a';
+
+while (x <= 'z')
+{
+	putchar(x);
+	putchar('\n');
+
+x++;
+}
